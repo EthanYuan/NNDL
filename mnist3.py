0f01bb31ab1b0656c65d3c105560527f402a2776
Ethan <yuanchengxing@hotmail.com>
2017-07-14 11:56:33 +0800

    add expand_mnist.py and update mnist3.py.

--- mnist3.py
+++ mnist3.py
@@ -1,14 +1,27 @@
 import network3
 from network3 import Network
 from network3 import ConvPoolLayer, FullyConnectedLayer, SoftmaxLayer
+from network3 import ReLU
 
-training_data, validation_data, test_data = network3.load_data_shared()
 mini_batch_size = 10
+
+expanded_training_data, validation_data, test_data = network3.load_data_shared(
+    "data/mnist_expanded.pkl.gz")
+
 net = Network([
     ConvPoolLayer(image_shape=(mini_batch_size, 1, 28, 28),
                   filter_shape=(20, 1, 5, 5),
-                  poolsize=(2, 2)),
-    FullyConnectedLayer(n_in=20 * 12 * 12, n_out=100),
-    SoftmaxLayer(n_in=100, n_out=10)],
+                  poolsize=(2, 2),
+                  activation_fn=ReLU),
+    ConvPoolLayer(image_shape=(mini_batch_size, 20, 12, 12),
+                  filter_shape=(40, 20, 5, 5),
+                  poolsize=(2, 2),
+                  activation_fn=ReLU),
+    FullyConnectedLayer(
+        n_in=40 * 4 * 4, n_out=1000, activation_fn=ReLU, p_dropout=0.5),
+    FullyConnectedLayer(
+        n_in=1000, n_out=1000, activation_fn=ReLU, p_dropout=0.5),
+    SoftmaxLayer(n_in=1000, n_out=10, p_dropout=0.5)],
     mini_batch_size)
-net.SGD(training_data, 60, mini_batch_size, 0.1, validation_data, test_data)
+net.SGD(expanded_training_data, 40, mini_batch_size, 0.03,
+        validation_data, test_data)
