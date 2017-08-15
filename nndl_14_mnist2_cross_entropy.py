6b1a06f8255e65473f18195ceb359ce27e4c30da
Ethan <yuanchengxing@hotmail.com>
2017-08-15 15:38:50 +0800

    add nndl_14 and nndl_15

---
+++ nndl_14_mnist2_cross_entropy.py
@@ -0,0 +1,13 @@
+import mnist_loader
+import network2
+
+training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
+
+net = network2.Network([784, 30, 10], cost=network2.CrossEntropyCost)
+net.large_weight_initializer()
+net.SGD(training_data, 30, 10, 3.0,
+        evaluation_data=validation_data,
+        monitor_evaluation_cost=True,
+        monitor_evaluation_accuracy=True,
+        monitor_training_cost=True,
+        monitor_training_accuracy=True)
